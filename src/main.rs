7ea10214f893aafd21cf980a23fe3116c133ad8b
sofa3376 <107646464+sofa3376@users.noreply.github.com>
2023-03-26 21:53:46 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use bkl::vm::{VM, Opcode};
+use std::io::{self, Write};
+
+use bkl::vm::VM;
+use bkl::interpreter::*;
 
 fn main() {
     let mut vm = VM::new();
     
-    // (5.5) / 2.2 * (1.0 - 2.0)
-    vm.code.push((Opcode::Push, 5.5)); // pushes 5 into stack
-    vm.code.push((Opcode::Push, 3.1)); // pushes 3 into stack
-    vm.code.push((Opcode::Add, 0.0)); // summates 5 and 3
-    vm.code.push((Opcode::Push, 2.2)); // pushes 2 into stack
-    vm.code.push((Opcode::Div, 0.0)); // divides 8 by 2
-    vm.code.push((Opcode::Push, 1.0)); // pushes 1 into stack
-    vm.code.push((Opcode::Push, 2.0)); // pushes 2 into stack
-    vm.code.push((Opcode::Sub, 0.0)); // substracts 2 from 1
-    vm.code.push((Opcode::Mul, 0.0)); // multiplates 4 by -1
+    println!("belkanLisp v0.0.1\nType your math expression or ^C to quit");
     
-    vm.execute();
-    let result = vm.pop();
-    println!("Result: {}", result); // -3.9090909090909087
+    loop {
+        // infix: (2 * 3) + (8 / 2)
+        // lisp (prefix): (+ (* 2 3) (/ 8 2))
+        // rpn: 2 3 * 8 2 / +
+        let mut expr = String::new();
+        print!("--> ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut expr)
+            .expect("Failed to read line");
+
+        match eval(&expr, &mut vm) {
+            Ok(result) => println!("Result: {}", result), // 10
+            Err(err) => println!("Error: {}", err),
+        }
+    }
 }
